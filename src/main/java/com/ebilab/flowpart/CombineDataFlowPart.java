17a90e0cccff9009038790647f8420febd40f51d
Mugicha <dainiti.sukita@gmail.com>
2018-02-24 10:11:20 +0900

    modify flow part (CombineDataFlowPart.java)

--- src/main/java/com/ebilab/flowpart/CombineDataFlowPart.java
+++ src/main/java/com/ebilab/flowpart/CombineDataFlowPart.java
@@ -11,10 +11,12 @@ import com.ebilab.operator.CombineDataOperatorFactory.CheckNikkei300;
 import com.ebilab.operator.CombineDataOperatorFactory.CheckTwitterStream;
 import com.ebilab.operator.CombineDataOperatorFactory.SetMissingValueToDowJones;
 import com.ebilab.operator.CombineDataOperatorFactory.SetMissingValueToNikkei;
+import com.ebilab.operator.CombineDataOperatorFactory.SetMissingValueToTwitter;
 import com.example.modelgen.dmdl.model.DailyDowJones;
 import com.example.modelgen.dmdl.model.DailyNikkei300;
 import com.example.modelgen.dmdl.model.DailyRateUsdJpy;
 import com.example.modelgen.dmdl.model.SummaryData;
+import com.example.modelgen.dmdl.model.SummaryTwitterStream;
 
 @FlowPart
 public class CombineDataFlowPart extends FlowDescription{
@@ -22,16 +24,19 @@ public class CombineDataFlowPart extends FlowDescription{
 	final In<DailyRateUsdJpy> dailyRateUsdJpy;
 	final In<DailyDowJones> dailyDowJones;
 	final In<DailyNikkei300> dailyNikkei300;
+	final In<SummaryTwitterStream> summaryTwitterStream;
 	final Out<SummaryData> summaryData;
 	
 	public CombineDataFlowPart(
 			In<DailyRateUsdJpy> dailyRateUsdJpy,
 			In<DailyDowJones> dailyDowJones,
 			In<DailyNikkei300> dailyNikkei300,
+			In<SummaryTwitterStream> summaryTwitterStream,
 			Out<SummaryData> summaryData) {
 		this.dailyRateUsdJpy = dailyRateUsdJpy;
 		this.dailyDowJones = dailyDowJones;
 		this.dailyNikkei300 = dailyNikkei300;
+		this.summaryTwitterStream = summaryTwitterStream;
 		this.summaryData = summaryData;
 	}
 	
@@ -39,14 +44,27 @@ public class CombineDataFlowPart extends FlowDescription{
 	protected void describe() {
 		CombineDataOperatorFactory operators = new CombineDataOperatorFactory();
 		
-		CheckDowJones checkDowJones = operators.checkDowJones(dailyDowJones, dailyRateUsdJpy);
 		
+		// 1. DOW JONES　の結合
+		CheckDowJones checkDowJones = operators.checkDowJones(dailyDowJones, dailyRateUsdJpy);
 		SetMissingValueToDowJones setMissingValueToDowJones = operators.setMissingValueToDowJones(checkDowJones.missed);
 		
-		CheckNikkei300 checkNikkei300 = operators.checkNikkei300(dailyNikkei300, checkDowJones.joined);
 		
+		// 2. Nikkei300 の結合
+		CheckNikkei300 checkNikkei300 = operators.checkNikkei300(dailyNikkei300, checkDowJones.joined);
+		checkNikkei300 = operators.checkNikkei300(dailyNikkei300, setMissingValueToDowJones.out);
 		SetMissingValueToNikkei setMissingValueToNikkei = operators.setMissingValueToNikkei(checkNikkei300.missed);
 		
-		CheckTwitterStream checkTwitterStream = operators.checkTwitterStream(twitterStream, checkNikkei300.joined);
+		
+		// 3. Twitter の結合
+		CheckTwitterStream checkTwitterStream = operators.checkTwitterStream(summaryTwitterStream, checkNikkei300.joined);
+		checkTwitterStream = operators.checkTwitterStream(summaryTwitterStream, setMissingValueToNikkei.out);
+		SetMissingValueToTwitter setMissingValueToTwitter = operators.setMissingValueToTwitter(checkTwitterStream.missed);
+		
+		
+		// 4. 出力
+		summaryData.add(checkTwitterStream.joined);
+		summaryData.add(setMissingValueToTwitter.out);
+		
 	}
 }
